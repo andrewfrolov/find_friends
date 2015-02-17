c8d51d0b659d152ddd2745b1784bd0670bb4d6bd
yuliyl <um@umneem.org>
2015-02-17 12:17:42 +0300

    Initial code.

--- prototype/vk/download.py
+++ prototype/vk/download.py
@@ -1,2 +1,36 @@
+#!/usr/bin/env python
+"""
+Vkontakte downloader module. Uses API.
+"""
 __author__ = 'Yuliy Lobarev'
 __name__='Vkontakte downloader (via API)'
+import urllib.request, urllib.error, urllib.parse
+import json
+# import sys
+
+def vk(meth, param):
+    """
+    VK API access function.
+    :param meth: Method to send to API.
+    :param param: User ID.
+    :return: Response from API.
+    """
+    url  = 'https://api.vk.com/method/%s' %meth
+    print(url)
+    method = {
+        'friends.get'    : 'user_id=%s',
+        'users.get'      : 'user_ids=%s' ,
+        'groups.get'     : 'user_id=%s' ,
+        'groups.getById' : 'group_ids=%s&fields=contacts,description,members_count',
+    }[meth] % param
+    print(method)
+    #connector  = urllib2.urlopen(url, method).read()
+    connector = urllib.request.urlopen(url, method).read()
+    data = json.loads( connector)
+
+    if 'error' in data:
+        print (data)
+        return list()
+    return data['response']
+
+
--- prototype/vk/tester.py
+++ prototype/vk/tester.py
@@ -1,2 +1,14 @@
+#!/usr/bin/env python
+"""
+Tester module for Vkontakte downloader.
+Uses download.py
+"""
 __author__ = 'Yuliy Lobarev'
 __name__='Tester module for Vkontakte downloader'
+from prototype.vk.download import vk
+#import sys
+#import os
+#import download
+#sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+print(vk('friends.get', 137589139))
