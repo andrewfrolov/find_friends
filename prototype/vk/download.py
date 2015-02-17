05c6eb9b8dbc8750f7b372b388e15cdca8083112
yuliyl <um@umneem.org>
2015-02-17 13:46:42 +0300

    Working code.

--- prototype/vk/download.py
+++ prototype/vk/download.py
@@ -3,34 +3,37 @@
 Vkontakte downloader module. Uses API.
 """
 __author__ = 'Yuliy Lobarev'
-__name__='Vkontakte downloader (via API)'
+__name__ = 'Vkontakte downloader (via API)'
 import urllib.request, urllib.error, urllib.parse
 import json
-# import sys
+import sys
 
-def vk(meth, param):
+def vk(meth, vkid):
     """
     VK API access function.
-    :param meth: Method to send to API.
-    :param param: User ID.
+    Don't forget to add time.sleep(0.36) between requests or VK will stop to respond!
+    :vkid meth: Method to send to API.
+    :vkid vkid: User ID.
     :return: Response from API.
     """
-    url  = 'https://api.vk.com/method/%s' %meth
-    print(url)
+    token = '123' # If group.get method is used
+    apiver = '5.28' # version of VK API
+    url = 'https://api.vk.com/method/%s' % meth
     method = {
-        'friends.get'    : 'user_id=%s',
-        'users.get'      : 'user_ids=%s' ,
-        'groups.get'     : 'user_id=%s' ,
-        'groups.getById' : 'group_ids=%s&fields=contacts,description,members_count',
-    }[meth] % param
-    print(method)
-    #connector  = urllib2.urlopen(url, method).read()
-    connector = urllib.request.urlopen(url, method).read()
-    data = json.loads( connector)
+                 'friends.get': 'user_id=%s',
+                 'users.get': 'user_ids=%s',
+                 'wall.get': 'owner_id=%s&count=20',
+                 'groups.get': 'user_id=%s&access_token=' + token, # Doesn't work without token...
+                 'groups.getById': 'group_ids=%s&fields=contacts,description,members_count',
+             }[meth] % vkid
 
+    connector = urllib.request.urlopen(url + '?' + method + '&v=' + apiver).read()
+    data = json.loads(connector.decode())
     if 'error' in data:
-        print (data)
+        print(data)
         return list()
     return data['response']
 
+def get_friends(vkid):
+    pass
 
--- prototype/vk/tester.py
+++ prototype/vk/tester.py
@@ -2,13 +2,24 @@
 """
 Tester module for Vkontakte downloader.
 Uses download.py
+Tests the main function calls,
+like vk('users.get', 137589139)
 """
 __author__ = 'Yuliy Lobarev'
 __name__='Tester module for Vkontakte downloader'
 from prototype.vk.download import vk
+import time
 #import sys
 #import os
 #import download
 #sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-print(vk('friends.get', 137589139))
+print(vk('users.get', 37740532))
+time.sleep(0.36)
+print(vk('friends.get', 37740532))
+time.sleep(0.36)
+# print(vk('groups.get', 150454703))
+# time.sleep(0.36)
+print(vk('groups.getById', 38463621))
+time.sleep(0.36)
+print(vk('wall.get', 37740532))
