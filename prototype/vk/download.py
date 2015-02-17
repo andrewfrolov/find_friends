eecea56c3666e5c3f92c8acbce2244ab35623041
yuliyl <um@umneem.org>
2015-02-17 14:45:20 +0300

    Tester produces JSON now as a test output.

--- prototype/vk/download.py
+++ prototype/vk/download.py
@@ -8,17 +8,20 @@ import urllib.request, urllib.error, urllib.parse
 import json
 import sys
 
-def vk(meth, vkid):
+def vk(meth, vkid = 0):
     """
     VK API access function.
     Don't forget to add time.sleep(0.36) between requests or VK will stop to respond!
     :vkid meth: Method to send to API.
     :vkid vkid: User ID.
-    :return: Response from API.
+    :return: Response from API as a dictionary. Check for 'items' key in the response.
     """
     token = '123' # If group.get method is used
     apiver = '5.28' # version of VK API
     url = 'https://api.vk.com/method/%s' % meth
+    if vkid == 0:
+        print('Sorry, no ID, exiting...')
+        return list()
     method = {
                  'friends.get': 'user_id=%s',
                  'users.get': 'user_ids=%s',
@@ -34,6 +37,19 @@ def vk(meth, vkid):
         return list()
     return data['response']
 
-def get_friends(vkid):
-    pass
+def getfriends(vkid = 0):
+    """
+    Function to get friends from a particular user by ID
+    :param vkid: User ID.
+    :return: List of friends IDs
+    """
+    if vkid == 0:
+        print('Sorry, no ID, exiting...')
+        return list()
+    userlist = (vk('friends.get', vkid))
+    if len(userlist) == 0:
+        print('Something happened, VK API returned an error for this user.')
+        print('Possibly the user prohibited access to the wall.')
+        return list()
+    return userlist['items']
 
--- prototype/vk/tester.py
+++ prototype/vk/tester.py
@@ -7,19 +7,26 @@ like vk('users.get', 137589139)
 """
 __author__ = 'Yuliy Lobarev'
 __name__='Tester module for Vkontakte downloader'
-from prototype.vk.download import vk
+from prototype.vk.download import vk, getfriends
 import time
+import json
+import io
 #import sys
 #import os
 #import download
 #sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+output = {}
+output['users.get'] = {37740532: vk('users.get', 37740532)}
+time.sleep(0.36)
+output['friends.get'] = {37740532: vk('friends.get', 37740532)}
+time.sleep(0.36)
+output['groups.getById'] = {38463621: vk('groups.getById', 38463621)}
+time.sleep(0.36)
+output['wall.get'] = {37740532: vk('wall.get', 37740532)}
+# print(vk('wall.get', 9393002)) Get an error, wall isn't accessible
+time.sleep(0.36)
+output['getfriends()'] = {37740532: getfriends(37740532)}
 
-print(vk('users.get', 37740532))
-time.sleep(0.36)
-print(vk('friends.get', 37740532))
-time.sleep(0.36)
-# print(vk('groups.get', 150454703))
-# time.sleep(0.36)
-print(vk('groups.getById', 38463621))
-time.sleep(0.36)
-print(vk('wall.get', 37740532))
+with open('tester.json', 'w') as f:
+    json.dump(output, f, indent=4)
+print('Hurray, something was dumped!')
